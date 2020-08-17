42920b0890d392bf76b8f3fb544002b21144d6db
Daniel A <dannyboy3385@gmail.com>
2020-08-17 13:32:31 -0700

    passing files through express

--- server.js
+++ server.js
@@ -1,5 +1,13 @@
 const express = require('express')
+const path = require('path')
 
 const app = express()
+
+app.get('/test', (req,res) => {
+  // res.send('hi')
+  // res.json({name: 'John Doe'})
+  res.sendFile(path.join(__dirname, 'public', 'index.html'))
+})
+
 
 app.listen(3000)
